c3eceb5c89e3d1ac8f8a0b67b902798b8e59b800
Boueny Folefack <bouenyfolefack@gmail.com>
2024-06-20 12:36:30 -0500

    edited replay script and added example rosbags

--- data/replay_SLAM.bash
+++ data/replay_SLAM.bash
@@ -1,4 +1,6 @@
 # roscore >/dev/null 2>&1 &
 rosparam set use_sim_time true
 roslaunch realsense2_camera opensource_tracking.launch offline:=true > /dev/null 2>&1 &
+trap 'rosnode kill --all' SIGINT
 rosbag play "$1" --clock
+sleep 3000h
